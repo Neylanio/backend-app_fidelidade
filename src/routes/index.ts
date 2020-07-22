4e23f303256005afdb88c63635542500f9883e10
Neylanio <neylaniogomes@gmail.com>
2020-07-22 18:03:01 -0300

    feat: Route configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import customersRouter from './customers.routes';
 
 const routes = Router();
 
-routes.use('/clientes', )
+routes.use('/clientes', customersRouter);
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+})
