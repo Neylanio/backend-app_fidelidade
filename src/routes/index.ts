1e99cc2cd6bbcb661b636ce2be3b83bb3a2662c2
Neylanio <neylaniogomes@gmail.com>
2020-07-22 20:34:36 -0300

    feat: add commitizen cli

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express';
+import { Router, request, response } from 'express';
 import customersRouter from './customers.routes';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.json({ok: true})
+});
 routes.use('/clientes', customersRouter);
 
 export default routes;
