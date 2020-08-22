4474c42e902aa0fe1b27d606e92900afcbe49c6d
Neylanio <neylaniogomes@gmail.com>
2020-08-21 22:41:50 -0300

    feat: start create customer with validators
    Using express-validator
    
    #10

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,11 +1,66 @@
-import Router, { json } from 'express';
-
-import { getRepository } from 'typeorm';
+import Router, { Request, Response } from 'express';
+import { validationResult, checkSchema } from 'express-validator';
+import CreateCustomerService from '../services/CreateCustomer';
 
 const customersRouter = Router();
 
 customersRouter.get('/', (request, response) => {
   return response.json({ ok: true });
 });
+
+customersRouter.post(
+  '/',
+  checkSchema({
+    email: {
+      isEmail: {
+        negated: false,
+      },
+    },
+    password: {
+      isLength: {
+        options: {
+          min: 6,
+        },
+      },
+    },
+  }),
+  async (request: Request, response: Response) => {
+    const { email, username, password, surname, whatsapp } = request.body;
+
+    const newUsername = username.toLowerCase();
+
+    try {
+      validationResult(request).throw();
+
+      const customerService = new CreateCustomerService();
+
+      const customer = await customerService.execute({
+        email,
+        username: newUsername,
+        password,
+        surname,
+        whatsapp,
+      });
+
+      return response.json(customer);
+    } catch (error) {
+      switch (error.param) {
+        case 'email':
+          return response.status(401).json({ message: 'Email inválido' });
+          break;
+
+        case 'password':
+          return response.status(401).json({ message: 'Senha inválida' });
+          break;
+
+        default:
+          return response
+            .status(401)
+            .json({ message: 'Erro ao tentar cadastrar' });
+          break;
+      }
+    }
+  },
+);
 
 export default customersRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,44 +1,9 @@
 import Router from 'express';
 
-import { getRepository } from 'typeorm';
-import { hash } from 'bcryptjs';
-import User from '../models/User';
-import Customer from '../models/Customer';
-
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  const { email, username, password, surname, whatsapp } = request.body;
-
-  const userRepository = getRepository(User);
-
-  const newPassword = await hash(password, 8);
-
-  const user = userRepository.create({
-    email,
-    username,
-    password: newPassword,
-    active: '1',
-  });
-
-  try {
-    const userId = await userRepository.save(user);
-
-    const customerRepository = getRepository(Customer);
-
-    const customer = customerRepository.create({
-      surname,
-      whatsapp,
-      active: '1',
-      user_id: userId.id,
-    });
-
-    await customerRepository.save(customer);
-
-    return response.send();
-  } catch (error) {
-    return response.status(401).json({ message: 'Error' });
-  }
+  return response.json({ message: true });
 });
 
 export default usersRouter;
