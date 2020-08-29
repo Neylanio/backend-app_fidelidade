3a3fb88e47e1d564703c411b7538b3743ee83e98
Neylanio <neylaniogomes@gmail.com>
2020-08-28 23:12:27 -0300

    feat: To Do Logon

--- src/routes/employees.routes.ts
+++ src/routes/employees.routes.ts
@@ -1,9 +1,12 @@
-import Router from 'express';
+import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import CreateEmployeeService from '../services/CreateEmployee';
 import User from '../models/User';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const employeesRouter = Router();
+
+employeesRouter.use(ensureAuthenticated);
 
 employeesRouter.get('/', async (request, response) => {
   const userRepository = getRepository(User);
@@ -30,21 +33,17 @@ employeesRouter.get('/', async (request, response) => {
 employeesRouter.post('/', async (request, response) => {
   const { email, username, password, name, type } = request.body;
 
-  try {
-    const employeeService = new CreateEmployeeService();
+  const employeeService = new CreateEmployeeService();
 
-    const employee = await employeeService.execute({
-      email,
-      username,
-      password,
-      name,
-      type,
-    });
+  const employee = await employeeService.execute({
+    email,
+    username,
+    password,
+    name,
+    type,
+  });
 
-    return response.json(employee);
-  } catch (error) {
-    return response.status(400).json({ message: error.message });
-  }
+  return response.json(employee);
 });
 
 export default employeesRouter;
